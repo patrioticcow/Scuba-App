2cd217dccc9c80063d1ca8670a8ab0be6b76d95c
Cristi Citea <cristi.catea@gmail.com>
2016-07-19 22:27:28 -0700

    'updates'

--- app/pages/questions/questions.ts
+++ app/pages/questions/questions.ts
@@ -1,23 +1,28 @@
 import {Component, ViewChild} from '@angular/core';
-import {App, Page, Slides, Alert, NavController, NavParams, List} from 'ionic-angular';
+import {App, Storage, SqlStorage, Page, Slides, Alert, NavController, NavParams, List} from 'ionic-angular';
 
 @Component({
 	templateUrl: 'build/pages/questions/questions.html'
 })
 export class Questions {
-	@ViewChild('questionSlideOptions') slider:Slides;
+	@ViewChild('questionSlideOptions') slider: Slides;
 	@ViewChild('button') button;
 
-	name:any;
-	data:any;
-	questions:any;
+	name: any;
+	data: any;
+	questions: any;
+	storage: Storage     = null;
 	questionSlideOptions = {
 		loop: false
 	};
 
-	constructor(private app:App, private navParams:NavParams) {
-		this.data = this.navParams.data;
+	constructor(private app: App, private navParams: NavParams) {
+		this.storage = new Storage(SqlStorage, {name: '_questions', backupFlag: SqlStorage.BACKUP_LOCAL, existingDatabase: false});
 
+		this.data = this.navParams.data;
+		this.storage.query('select * from questions').then((resp) => {
+			console.log(resp);
+		});
 		this.getQuestions();
 	}
 
@@ -26,6 +31,7 @@ export class Questions {
 	}
 
 	getQuestions() {
+		console.log(this.data.data);
 		this.questions = this.randomizeAnswers(this.data.data);
 		this.name      = this.data.name;
 	}
@@ -51,7 +57,11 @@ export class Questions {
 		let buttonEl = button._elementRef.nativeElement;
 		let isCorrect;
 
+		let buttons = document.getElementsByClassName('answer' + id);
+
 		for (let i = 0; i < this.questions.length; i++) {
+			buttonEl.setAttribute('disabled', 'true');
+
 			if (id === this.questions[i].id) {
 				if (answer === this.questions[i].answer) {
 					buttonEl.className = 'button-green';
@@ -61,9 +71,16 @@ export class Questions {
 					isCorrect          = false;
 				}
 
-				console.warn(this.questions[i]);
-				console.warn(isCorrect);
+				//let key   = this.questions[i].key + '_' + this.questions[i].group;
+				//let value = JSON.stringify({question: this.questions[i].id, group: this.questions[i].group, correct: isCorrect});
+
+				this.storage.query('insert into questions(question, group, correct) values("' + this.questions[i].id + '", "' + this.questions[i].group + '", "' + isCorrect + '")');
 			}
+		}
+
+		for (let j = 0; j < buttons.length; j++) {
+			console.log(buttons[j]);
+			buttons[j].setAttribute('disabled', 'true');
 		}
 	}
 }
